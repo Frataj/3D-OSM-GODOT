406df6bc5e8fed266530175a492aecd4b9a7acf8
Fabian <fabian.freitag@ost.ch>
2023-11-04 10:36:09 +0100

    changed tile loading to use tiles from http request instead of files from filesystem some functions need reworking Water and waterways are now combined -> needs reworking some landcover seems to be missing

--- code/main.gd
+++ code/main.gd
@@ -8,17 +8,26 @@ const ROADS = preload("res://src/roads.gd")
 
 # key represents the height
 const polygon_types = {
-	10: ["building", Color(211, 211, 211, 255)],
-	-1: ["landcover", Color(0, 255, 0, 255)],
-	2: ["water", Color(0, 0, 255, 255)],
+	10: ["buildings", Color(211, 211, 211, 255)],
+	-1: ["common", Color(0, 255, 0, 255)],
+#	2: ["water", Color(0, 0, 255, 255)],
 }
 
 func _ready():
-	var start_x = 34317
-	var start_y = 42582
-	var start_tile_path = "res://tiles/switzerland/16/" + str(start_x) + "/" + str(start_y) + ".pbf"
-	
-	var tile = MVT.load_tile_gz(start_tile_path)
+	var start_x = 34373
+	var start_y = 22991
+
+	var tile_http_link = "https://tiles.streets.gl/vector/16/" + str(start_x) + "/" + str(start_y)
+	var downloaded_tile = "res://tiles/" + str(start_x) + str(start_y)
+	$HTTPRequest.download_file = downloaded_tile
+	$HTTPRequest.request_completed.connect(_on_request_completed)
+	$HTTPRequest.request(tile_http_link)
+
+func _on_request_completed(result, response_code, headers, body):
+	var tile = MVT.load_tile($HTTPRequest.download_file)
+	print("--------------------")
+	for layer in tile.layers():
+		print(layer.name())
 	
 	for height in polygon_types.keys():
 		var polygon_type_data = polygon_types[height]
@@ -26,14 +35,14 @@ func _ready():
 		for polygon in polygons:
 			var color = polygon_type_data[1]
 			add_child(CALCULATE_POLYGON.generate_polygon(polygon, height, color))
-			
+
 	for layer in tile.layers():
-		if layer.name() == "transportation":
+		if layer.name() == "highways":
 			for feature in layer.features():
 				ROADS.generate_paths(ROADS.build_road_geometries(feature.geometry()), self, Color(0,0,0,255))
-		if layer.name() == "waterway":
+		if layer.name() == "water":
 			for feature in layer.features():
 				ROADS.generate_paths(ROADS.build_road_geometries(feature.geometry()), self, Color(0,0,255,255))
-				
+
 func _process(delta):
 	pass
