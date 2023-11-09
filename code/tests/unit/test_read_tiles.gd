d761e485a7e7de32e7c0a1b7889b7e59136c22a5
Josip Di Benedetto <josip.dibenedetto@ost.ch>
2023-11-09 19:12:37 +0100

    created unit test for tile reader

--- code/tests/unit/test_read_tiles.gd
+++ code/tests/unit/test_read_tiles.gd
@@ -2,11 +2,21 @@ extends "res://addons/gut/test.gd"
 
 const MVT = preload("res://addons/geo-tile-loader/vector_tile_loader.gd")
 
+const LAYERS = []
+
 func get_tile():
 	return MVT.load_tile("res://tests/tiles/3437322991")
 
 
 func test_read_layer():
 	var tile = get_tile()
+	assert_not_null(tile, "Failed to load the tile")
+	assert(tile.layers().size() > 0, "No layers found in the tile")
+	
+	var layer = tile.layers()[0]
+	assert_not_null(layer, "Failed to get the layer")
+	
 	var layer_name = tile.layers()[0].name()
-	assert_eq(layer_name, "point", "Should have been point")
+	assert(layer_name != "", "Layer name is empty")
+	
+	
