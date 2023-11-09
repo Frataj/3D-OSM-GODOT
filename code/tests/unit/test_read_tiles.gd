99fb9b9b4ef6d90d65ae04cf75eaf7944ac7a3b3
Josip Di Benedetto <josip.dibenedetto@ost.ch>
2023-11-09 13:58:50 +0100

    modified initial test

--- code/tests/unit/test_read_tiles.gd
+++ code/tests/unit/test_read_tiles.gd
@@ -1,5 +1,12 @@
 extends "res://addons/gut/test.gd"
 
-func test_equality():
-	var variable = "apples";
-	assert_eq(variable, "apples", "Should've been apples")
+const MVT = preload("res://addons/geo-tile-loader/vector_tile_loader.gd")
+
+func get_tile():
+	return MVT.load_tile("res://tests/tiles/3437322991")
+
+
+func test_read_layer():
+	var tile = get_tile()
+	var layer_name = tile.layers()[0].name()
+	assert_eq(layer_name, "point", "Should have been point")
